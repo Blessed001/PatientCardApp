e2cb2a5d8d58d5d59f4f2f8c5fe677312a0900e0
Dionisio Chiquengue <claudiojoc46@hotmail.com>
2020-03-27 00:47:19 +0300

    +

--- PatientCardApp.UI/Data/PatientCardDataServices.cs
+++ PatientCardApp.UI/Data/PatientCardDataServices.cs
@@ -1,9 +1,6 @@
 ﻿using PatientCardApp.Model;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace PatientCardApp.UI.Data
 {
---
+++ PatientCardApp.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using PatientCardApp.Model;
+using PatientCardApp.UI.Data;
+using System.Collections.ObjectModel;
+
+
+namespace PatientCardApp.UI.ViewModel
+{
+    class MainViewModel
+    {
+        private readonly IPatientCardDataServices _patientCardDataServices;
+        private PatientCard _selectedPatient;
+
+
+        public MainViewModel(IPatientCardDataServices patientCardDataServices)
+        {
+            PatientCards = new ObservableCollection<PatientCard>();
+            _patientCardDataServices = patientCardDataServices;
+        }
+
+        public void Load()
+        {
+            var patientCards = _patientCardDataServices.GetAll();
+            PatientCards.Clear();
+
+            foreach(var patientCard in patientCards)
+            {
+                PatientCards.Add(patientCard);
+            }
+        }
+        public ObservableCollection<PatientCard> PatientCards  { get; set; }
+
+        public PatientCard SelectedPatient
+        {
+            get { return _selectedPatient; }
+            set { _selectedPatient = value; }
+        }
+
+    }
+}
