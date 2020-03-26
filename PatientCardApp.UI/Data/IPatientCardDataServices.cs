9860c282395eda80301719eea41d18767d3feb40
Dionisio Chiquengue <claudiojoc46@hotmail.com>
2020-03-27 00:18:09 +0300

    Added and implemented IPatientCardDataServices

---
+++ PatientCardApp.UI/Data/IPatientCardDataServices.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using PatientCardApp.Model;
+
+namespace PatientCardApp.UI.Data
+{
+    public interface IPatientCardDataServices
+    {
+        IEnumerable<PatientCard> GetAll();
+    }
+}
---
+++ PatientCardApp.UI/Data/PatientCardDataServices.cs
@@ -0,0 +1,20 @@
+﻿using PatientCardApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientCardApp.UI.Data
+{
+    public class PatientCardDataServices : IPatientCardDataServices
+    {
+        public IEnumerable<PatientCard> GetAll()
+        {
+            yield return new PatientCard { FirstName = "Patient 1", LastName = "Patient Last 1" };
+            yield return new PatientCard { FirstName = "Patient 2", LastName = "Patient Last 2" };
+            yield return new PatientCard { FirstName = "Patient 3", LastName = "Patient Last 3" };
+            yield return new PatientCard { FirstName = "Patient 4", LastName = "Patient Last 4" };
+        }
+    }
+}
