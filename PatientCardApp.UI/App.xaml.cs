609c0acfcbd857ce6f9f70cc4a4142dfa1afc98c
Dionisio Chiquengue <claudiojoc46@hotmail.com>
2020-03-27 12:39:26 +0300

    Defined the UI

--- PatientCardApp.UI/App.xaml.cs
+++ PatientCardApp.UI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PatientCardApp.UI.Data;
+using PatientCardApp.UI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,12 @@ namespace PatientCardApp.UI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow(
+                new MainViewModel(
+                    new PatientCardDataServices()));
+            mainWindow.Show();
+        }
     }
 }
