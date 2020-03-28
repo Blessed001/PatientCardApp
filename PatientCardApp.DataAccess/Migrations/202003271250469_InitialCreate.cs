ed715c04ac8aca0f00833f589c6ab6aa1729e265
Dionisio Chiquengue <claudiojoc46@hotmail.com>
2020-03-28 07:22:33 +0300

    Added constrains+, Created and seed the database

--- PatientCardApp.DataAccess/Migrations/202003271250469_InitialCreate.cs
+++ PatientCardApp.DataAccess/Migrations/202003271250469_InitialCreate.cs
@@ -12,13 +12,13 @@ namespace PatientCardApp.DataAccess.Migrations
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
-                        FirstName = c.String(nullable: false, maxLength: 50),
-                        MidleName = c.String(maxLength: 50),
-                        LastName = c.String(maxLength: 50),
-                        Gender = c.String(),
+                        FirstName = c.String(nullable: false, maxLength: 100),
+                        MidleName = c.String(nullable: false, maxLength: 100),
+                        LastName = c.String(nullable: false, maxLength: 100),
+                        Gender = c.String(nullable: false),
                         BirthDay = c.DateTime(nullable: false),
-                        Address = c.String(),
-                        PhoneNumber = c.Int(nullable: false),
+                        Address = c.String(nullable: false, maxLength: 150),
+                        PhoneNumber = c.String(nullable: false, maxLength: 20),
                     })
                 .PrimaryKey(t => t.Id);
             
--- PatientCardApp.Model/PatientCard.cs
+++ PatientCardApp.Model/PatientCard.cs
@@ -7,20 +7,32 @@ namespace PatientCardApp.Model
     {
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(50)]
+        [Required(ErrorMessage = "Поля {0} является обязательным")]
+        [MaxLength(100, ErrorMessage = "Поля {0} должен быть максимуме {1} символи")]
         public string FirstName { get; set; }
 
-        [StringLength(50)]
+        [Required(ErrorMessage = "Поля {0} является обязательным")]
+        [MaxLength(100, ErrorMessage = "Поля {0} должен быть максимуме {1} символи")]
         public string MidleName { get; set; }
 
-        [StringLength(50)]
+        [Required(ErrorMessage = "Поля {0} является обязательным")]
+        [MaxLength(100, ErrorMessage = "Поля {0} должен быть максимуме {1} символи")]
         public string LastName { get; set; }
 
-
+        [Required(ErrorMessage = "Поля {0} является обязательным")]
         public string Gender { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
         public DateTime BirthDay { get; set; }
+        [Required(ErrorMessage = "Поля {0} является обязательным")]
+        [MaxLength(150, ErrorMessage = "Поля {0} должен быть максимуме {1} символи")]
         public string Address { get; set; }
-        public int PhoneNumber { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(20, ErrorMessage = "Поля {0} должен быть максимуме {1} символи")]
+        [Required(ErrorMessage = "Поля {0} является обязательным")]
+        public string PhoneNumber { get; set; }
     }
 }
